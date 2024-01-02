f8e8d5529c031383803a0fcad41776acb128958c
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-01-02 15:17:20 +0600

    [COMPLETE SETUP A NEXT.JS PROJECT WITH SHADCN-UI, TYPESCRIPT, ESLINT, PRETTIER, HUSKY]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home: React.FC = () => {
-  return <div>page</div>
+  return (
+    <div>
+      <Button variant="secondary">Secondary</Button>
+      <ModeToggle />
+    </div>
+  )
 }
 
 export default Home
